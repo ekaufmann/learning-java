cdb8b7fc7c84ccf3ac05ae7fb198defc8dccea2b
Eduardo <51715734+ekaufmann@users.noreply.github.com>
2019-11-23 21:22:57 -0200

    Basic concepts - Data input

---
+++ src/com/eduardo/basicconcepts/LerDados.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LerDados {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        /*System.out.print("Digite seu nome completo: ");
+        String nomeCompleto = scan.nextLine();
+        System.out.println("Seu nome é " + nomeCompleto);
+
+        System.out.println("Digite seu primeiro nome:");
+        String primeiroNome = scan.next();
+        System.out.println("Seu primeiro nome é " + primeiroNome); */
+
+        System.out.print("Digite seu primeiro nome, sua idade, quanto quer ganhar e um objetivo de vida: ");
+        String primeiroNome = scan.next();
+        int idade = scan.nextInt();
+        double salario = scan.nextDouble();
+        String objetivo = scan.nextLine();
+
+        System.out.println(primeiroNome + " tem " + idade + " anos e quer ganhar R$ " + salario);
+        System.out.println("Um de seus objetivos é: " + objetivo);
+
+        scan.close();
+    }
+}
