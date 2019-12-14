6346656683b34bcaebb61edf60df4e73b14b895e
Eduardo <51715734+ekaufmann@users.noreply.github.com>
2019-12-14 13:24:30 -0300

    Multi Arrays

--- src/com/eduardo/multiarray/Matrices.java
+++ src/com/eduardo/multiarray/Matrices.java
@@ -17,14 +17,52 @@ public class Matrices {
 		notas[1][2] = 7.4;
 		notas[1][3] = 8.5;
 		
+		// using index
 		for (int i = 0; i < notas.length; i++) {
-			//System.out.print(notas[i] + " "); // This prints the memory position of each element.
+			// System.out.print(notas[i] + " "); // This prints the memory position of each element.
 			
 			for (int j = 0; j < notas[i].length; j++) {
 				System.out.print(notas[i][j] + " ");
 			}
 			
 			System.out.println();
+		}
+		
+		System.out.println("\nMédia por aluno:");
+		
+		double soma, qtdNotas, index;
+		for (int i = 0; i < notas.length; i++) {
+			
+			soma = 0;
+			qtdNotas = notas[i].length;
+			
+			for (int j = 0; j < notas[i].length; j++) {
+				soma += notas[i][j];
+			}
+			System.out.println("Aluno " + (i + 1) + ": " + (soma / qtdNotas));
+		}
+		
+		System.out.println();
+		
+		// using for-each
+		for (double[] aluno : notas) {
+			// System.out.println(aluno);
+			for (double nota : aluno) {
+				System.out.print(nota + " ");
+			}
+			System.out.println();
+		}
+		
+		for (double[] aluno : notas) {
+			soma = 0;
+			qtdNotas = aluno.length;
+			index = 1;
+			
+			for (double nota : aluno) {
+				soma += nota;
+			}
+			System.out.printf("Aluno %.0f: %.2f\n", index, (soma / qtdNotas));
+			index++;
 		}
 		
 	}
