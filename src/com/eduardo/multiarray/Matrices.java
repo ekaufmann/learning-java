fdef0f323b3ef9f14d1a75af0e903f3af736941b
Eduardo <51715734+ekaufmann@users.noreply.github.com>
2019-12-14 12:40:20 -0300

    Multidimensional Arrays

---
+++ src/com/eduardo/multiarray/Matrices.java
@@ -0,0 +1,22 @@
+package com.eduardo.multiarray;
+
+
+public class Matrices {
+	
+	public static void main (String[] args) {
+		
+		double[][] notas = new double[5][4]; // [lines][columns]
+		
+		notas[0][0] = 10;
+		notas[0][1] = 8.6;
+		notas[0][2] = 6.7;
+		notas[0][3] = 8;
+		
+		notas[0][0] = 10;
+		notas[0][1] = 8.6;
+		notas[0][2] = 6.7;
+		notas[0][3] = 8;
+		
+	}
+}
+
