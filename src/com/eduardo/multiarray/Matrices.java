2c9105105d10cb73a33063ebbf5d8e7fa0df6d13
Eduardo <51715734+ekaufmann@users.noreply.github.com>
2019-12-14 12:58:11 -0300

    Multi Arrays

--- src/com/eduardo/multiarray/Matrices.java
+++ src/com/eduardo/multiarray/Matrices.java
@@ -12,10 +12,20 @@ public class Matrices {
 		notas[0][2] = 6.7;
 		notas[0][3] = 8;
 		
-		notas[0][0] = 10;
-		notas[0][1] = 8.6;
-		notas[0][2] = 6.7;
-		notas[0][3] = 8;
+		notas[1][0] = 9;
+		notas[1][1] = 8.2;
+		notas[1][2] = 7.4;
+		notas[1][3] = 8.5;
+		
+		for (int i = 0; i < notas.length; i++) {
+			//System.out.print(notas[i] + " "); // This prints the memory position of each element.
+			
+			for (int j = 0; j < notas[i].length; j++) {
+				System.out.print(notas[i][j] + " ");
+			}
+			
+			System.out.println();
+		}
 		
 	}
 }
