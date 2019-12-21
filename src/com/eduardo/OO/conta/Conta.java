6d3834cd841694177138f13376046be5917ffd44
Eduardo <51715734+ekaufmann@users.noreply.github.com>
2019-12-21 16:50:18 -0300

    OO - Exercises

--- src/com/eduardo/OO/conta/Conta.java
+++ src/com/eduardo/OO/conta/Conta.java
@@ -5,53 +5,57 @@ import java.util.Scanner;
 public class Conta {
 	
 	Scanner input = new Scanner(System.in);
-	String numeroConta;
-	String agencia;
-	String acao;
+	String numeroConta, agencia, acao;
 	double saldo, limiteAtual, limiteOriginal, aux;
 	boolean especial;
 	
 	void sacar(double saque) {
+		System.out.println("\n=== SAQUE ===\n");
 		
-		if (saque < saldo) {
-			System.out.println("\nSaldo = " + saldo);
+		if (saque <= saldo) {
+			System.out.println("Saldo = " + saldo);
 			saldo -= saque;
-			System.out.println("\nSaque realizado com sucesso!");
+			System.out.println("Saque realizado com sucesso!");
 			System.out.println("Valor sacado = " + saque);
 			System.out.println("Saldo atual = " + saldo);
 				
-		} else if (saque < (saldo + limiteAtual)) {
-			System.out.println("Saldo da conta = " + saldo);
-			System.out.println("Quer utilizar o cheque especial? S-Sim ou N-Não");
-			acao = input.next().toUpperCase();
-			
-			if (acao.equals("S")) {
-				aux = saque - saldo;
-				System.out.println("Uso do limite autorizado.\n\nSeu limite atual é " + limiteAtual);
-				System.out.printf("Será usado R$ %.2f do limite de R$ %.2f.\n", aux, limiteAtual);
-				
-				saldo = 0;
-				limiteAtual -= aux;				
-				
-				System.out.println("\nSaque realizado com sucesso!");
-				System.out.println("Valor sacado = " + saque);
-				System.out.println("Saldo atual = " + saldo);
-				System.out.println("Limite restante = " + limiteAtual);
-			} else {
-				System.out.println("Uso do limite não autorizado!");
-			}
-			
 		} else {
-			System.out.println("Saldo insuficiente!");
-			System.out.println("Saldo = " + saldo);
-			System.out.println("Valor de saque = " + saque);
+			if(especial) {
+				if (saque < (saldo + limiteAtual)) {
+					System.out.println("Saldo da conta = " + saldo);
+					System.out.println("Quer utilizar o cheque especial? S-Sim ou N-Não");
+					acao = input.next().toUpperCase();
+				
+					if (acao.equals("S")) {
+						aux = saque - saldo;
+						System.out.println("Uso do limite autorizado.\n\nSeu limite atual é " + limiteAtual);
+						System.out.printf("Será usado R$ %.2f do limite de R$ %.2f.\n", aux, limiteAtual);
+						
+						saldo = 0;
+						limiteAtual -= aux;				
+						
+						System.out.println("\nSaque realizado com sucesso!");
+						System.out.println("Valor sacado = " + saque);
+						System.out.println("Saldo atual = " + saldo);
+						System.out.println("Limite restante = " + limiteAtual);
+					} else {
+						System.out.println("Uso do limite não autorizado!");
+					}
+				}
+
+			} else {
+					System.out.println("Saldo insuficiente!");
+					System.out.println("Saldo = " + saldo);
+					System.out.println("Valor de saque = " + saque);
+			}
 		}
 	}
 	
 	void depositar(double deposito) {
+		System.out.println("\n=== DEPÓSITO ===\n");
 		
-		System.out.println("\nSaldo anterior = " + saldo);
-		System.out.println("\nDepositou = " + deposito);
+		System.out.println("Saldo anterior ao depósito = " + saldo);
+		System.out.println("Depositou = " + deposito);
 		
 		if (limiteAtual < limiteOriginal) {
 			aux = limiteOriginal - limiteAtual;
--- src/com/eduardo/OO/conta/ContaTeste.java
+++ src/com/eduardo/OO/conta/ContaTeste.java
@@ -9,6 +9,7 @@ public class ContaTeste {
 		conta1.saldo = 130;
 		conta1.limiteOriginal = 350;
 		conta1.limiteAtual = conta1.limiteOriginal;
+		conta1.especial = true;
 		
 		conta1.sacar(245);
 		conta1.depositar(670);
